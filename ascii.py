895c9b56fa910fe5e28b79be879e9ea502ae42a9
Alejandra Gomez <alejagomezm06@gmail.com>
2024-05-24 17:34:59 -0500

    Se edita la funcion ascii

--- ascii.py
+++ ascii.py
@@ -1,8 +1,8 @@
 def ascii(binary_string):
-    decimal_value = int(binary_string, 2)
+    valorDecimal = int(binary_string, 2)
     
-    ascii_char = chr(decimal_value)
+    caracterAscii = chr(valorDecimal)
 
-    hex_value = hex(decimal_value)[2:]  
+    valorHexagecimal = hex(valorDecimal)[2:]  
     
-    return f"{ascii_char}-{hex_value}"
+    return f"{caracterAscii}-{valorHexagecimal}"
