4769a3f144fa81df53dfdbd66fdba7b8bd60237d
Alejandra Gomez <alejagomezm06@gmail.com>
2024-05-24 17:17:48 -0500

    Se agregan cambios a la funcion caracter y el menu principal

---
+++ menu.py
@@ -0,0 +1,25 @@
+from caracter import caracter
+from ascii import ascii
+def menu():
+    while True:
+        print('1. Generar la representación en byte de un carácter\n'
+            '2. Generar la representación en byte de una palabra\n'
+            '3. Generar la representación ASCII de un byte\n'
+            '4. Salir\n')
+        opcion = int(input('Digite la opcion que desea: '))
+        if opcion == 1:
+            letra = input('Digite la letra ')
+            print(caracter(letra))
+        elif opcion == 2:
+            palabra = input('Digite la palabra ')
+            print(caracter(palabra))
+        elif opcion == 3:
+            codigo = input('Digite el código ')
+            print(ascii(codigo))
+        elif opcion == 4:
+            break
+        else:
+            print('Digite un número valido')
+
+menu()
+    
